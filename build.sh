02f6359a9862f97558c4505e5f508766b25da2f7
maxregnerisch <113422283+maxregnerisch@users.noreply.github.com>
2023-04-08 17:48:08 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+!/bin/bash
 set -ex
 #export VERSION=$(cat debian/changelog | head -n 1 | sed "s/.*(//g" | sed "s/).*//g")
 export VERSION=$(curl https://kernel.org/ | grep "downloadarrow_small.png" | sed "s/.*href=\"//g;s/\".*//g;s/.*linux-//g;s/\.tar.*//g")
 if echo ${VERSION} | grep -e "\.[0-9]*\.0$" ; then
-    export VERSION=${VERSION::-2}
+    export VERSION=${VERSION::-3}
+elif echo ${VERSION} | grep -e "\.[0-9]*\.[0-9]*\.0$" ; then
+    export VERSION=${VERSION::-4
 fi
 # Stage 0: set version
 sed -i "s/9999/${VERSION}/g" debian/changelog
@@ -11,6 +13,8 @@ sed -i "s/9999/${VERSION}/g" debian/changelog
 # fetch source
 wget https://gitlab.com/turkman/devel/sources/mklinux/-/raw/master/mklinux.sh -O mklinux
 mkdir -p ./debian/linux
-ALLOWROOT=1 bash mklinux -i -o "./debian/linux" -t linux -c "https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/trunk/config" -y 1
+ALLOWROOT=1 bash mklinux -i -o "./mros/linux" -t linux -c "https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/trunk/config" -y 1
 # decompress for initramfs-tools
 find ./debian/linux -type f -iname "*.ko.zst" -exec zstd -d --rm {} \;
+
+
