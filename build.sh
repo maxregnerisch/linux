63ff71e47a0e8b3c842f5dccd2075a0f75753301
maxregnerisch <113422283+maxregnerisch@users.noreply.github.com>
2023-04-08 17:03:16 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 set -ex
 #export VERSION=$(cat debian/changelog | head -n 1 | sed "s/.*(//g" | sed "s/).*//g")
-export VERSION=$(curl https://kernel.org/ | grep "downloadarrow_small.png" | sed "s/.*href=\"//g;s/\".*//g;s/.*linux-//g;s/\.tar.*//g")
-if echo ${VERSION} | grep -e "\.[0-9]*\.0$" ; then
-    export VERSION=${VERSION::-2}
-fi
-# Stage 0: set version
-sed -i "s/9999/${VERSION}/g" debian/changelog
-# Stage 1: Get version and fetch source code
-# fetch source
-wget https://gitlab.com/turkman/devel/sources/mklinux/-/raw/master/mklinux.sh -O mklinux
-mkdir -p ./debian/linux
-ALLOWROOT=1 bash mklinux -i -o "./debian/linux" -t linux -c "https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/trunk/config" -y 1
-# decompress for initramfs-tools
-find ./debian/linux -type f -iname "*.ko.zst" -exec zstd -d --rm {} \;
+export MROS_VERSION=1.0
+# Stage 1: build
+docker build --build-arg VERSION=${VERSION} -t mros-build:${MROS_VERSION} .
+# Stage 2: create container
+docker create --name mros-build-${MROS_VERSION} mros-build:${MROS_VERSION}
+# Stage 3: copy files
+docker cp mros-build-${MROS_VERSION}:/build/linux-image-${VERSION}-mros-${MROS_VERSION}.deb .
+docker cp mros-build-${MROS_VERSION}:/build/linux-headers-${VERSION}-mros-${MROS_VERSION}.deb .
+# Stage 4: cleanup
+docker rm mros-build-${MROS_VERSION}
+docker rmi mros-build:${MROS_VERSION}
+# Stage 5: rebrand files
+mv linux-image-${VERSION}-mros-${MROS_VERSION}.deb linux-image-${VERSION}-mros-${MROS_VERSION}-generic_${MROS_VERSION}_amd64.deb
+mv linux-headers-${VERSION}-mros-${MROS_VERSION}.deb linux-headers-${VERSION}-mros-${MROS_VERSION}-generic_${MROS_VERSION}_amd64.deb
+# Stage 6: sign files
+gpg --detach-sign linux-image-${VERSION}-mros-${MROS_VERSION}-generic_${MROS_VERSION}_amd64.deb
+gpg --detach-sign linux-headers-${VERSION}-mros-${MROS_VERSION}-generic_${MROS_VERSION}_amd64.deb
+# Stage 7: upload files
+scp linux-image-${VERSION}-mros-${MROS_VERSION}-generic_${MROS_VERSION}_amd64.deb*
+scp linux-headers-${VERSION}-mros-${MROS_VERSION}-generic_${MROS_VERSION}_amd64.deb*
+# Stage 8: cleanup
+rm linux-image-${VERSION}-mros-${MROS_VERSION}-generic_${MROS_VERSION}_amd64.deb*
+rm linux-headers-${VERSION}-mros-${MROS_VERSION}-generic_${MROS_VERSION}_amd64.deb*
